303c45aa879ea14a0815a4a8b66082ab5a7f261b
Radosław Wieczorek <radoslaw.wieczorek24@gmail.com>
2015-05-20 22:05:08 +0200

    Finish 1st version search, updated seeds.rb, added flight search results as radio buttons

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,19 +1,52 @@
 class Flight < ActiveRecord::Base
-  validates :from_airport, presence: true
-  validates :to_airport, presence: true
+  validates :from_airport,   presence: true
+  validates :to_airport,     presence: true
   validates :start_datetime, presence: true 
-  validates :duration, presence: true
+  validates :duration,       presence: true
 
   belongs_to :from_airport, class_name: "Airport"
-  belongs_to :to_airport, class_name: "Airport"
+  belongs_to :to_airport,   class_name: "Airport"
 
-  def self.get_start_datetimes
-    Flight.all.collect{ |f| f.start_datetime.to_date }.uniq
+  def self.get_start_dates
+    Flight.all.map{ |f| f.start_datetime.to_date }.uniq
   end
 
   def self.search(query)
-    where("from_airport_id LIKE ? AND to_airport_id LIKE ?",
-          "#{query[:from_airport_id]}", "#{query[:to_airport_id]}")
+    conditions = {}
+    conditions[:from_airport_id] = query[:from_airport_id] unless query[:from_airport_id].blank?
+    conditions[:to_airport_id]   = query[:to_airport_id]   unless query[:to_airport_id].blank?
+    unless query[:start_datetime].blank?
+      parsed_date = Date.parse(query[:start_datetime])
+      conditions[:start_datetime]  = parsed_date.beginning_of_day..parsed_date.end_of_day
+    end
+    Flight.where(conditions)
   end
-
 end
+
+# Try the methods below, they can prove faster when implemented correctly
+
+
+#   def self.search(attributes)
+#     supported_filters = [:from_airport_id, :to_airport_id]
+#     attributes.slice(*supported_filters).inject(self) do |scope, (key, value)|
+#       value.present? ? scope.send(key, value) : scope
+#     end  
+#   end
+# end
+
+# def self.filter(attributes)
+#     attributes.inject(self) do |scope, (key, value)|
+#       return scope if value.blank?
+#       case key.to_sym
+#       when :id, :zip # direct search
+#         scope.where(key => value)
+#       when :city, :state # regexp search
+#         scope.where(["#{key} ILIKE ?", "%#{value}%"])
+#       when :order # order=field-(ASC|DESC)
+#         attribute, order = value.split("-") 
+#         scope.order("#{self.table_name}.#{attribute} #{order}")
+#       else # unknown key (do nothing or raise error, as you prefer to)
+#         scope
+#       end 
+#     end  
+#   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,36 @@
 codes = ['NYC', 'LAX', 'WAW', 'ALW', 'WSZ', 'VLL', 'TLS', 'TYO','FSD', 'STI',
          'ACY', 'BAU', 'BJS', 'CTA', 'DYG']
 
+dates = (Date.today.to_date..(Date.today+30.days).to_date)
+
 def rand_in_range(from, to)
   rand * (to - from) + from
 end
 
-def rand_time(from=Time.zone.now, to)
-  Time.at(rand_in_range(from.to_f, to.to_f))
+def rand_date(from=Time.zone.now, to)
+  Time.at(rand_in_range(from.to_f, to.to_f)).to_date
+end
+
+def random_hour
+  (Date.today + rand(0..24).hour + rand(0..60).minutes).to_datetime
+end
+
+def airport_ids
+  Airport.all.map { |a| a.id }
 end
 
 codes.each do |code|
   Airport.create(code: code)
 end
 
-def random_to_airport(from_airport)
-  Airport.all.select{ |a| a.id != from_airport }.map{ |a| a.id }.sample
-end
-
-15.times do
+dates.each do |date|
   Airport.all.each do |airport|
-    Flight.create(from_airport_id: airport.id, to_airport_id: random_to_airport(airport.id),
-                  start_datetime: rand_time(Time.zone.now + 6.months), 
-                  duration: rand_in_range(7200, 32000))
+    2.times do
+      airport_id_a = [airport.id]
+      Flight.create(from_airport_id: airport.id,
+                    to_airport_id:   (airport_ids - airport_id_a).sample,
+                    start_datetime:  (date+rand(0..24).hours),
+                    duration:        rand_in_range(7200, 32000))
+    end
   end
 end
